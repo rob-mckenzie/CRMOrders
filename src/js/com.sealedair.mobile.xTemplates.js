769e51916eebc8e7fa2a46ec5a12facf88c4948d
Rob McKenzie <rob.mckenzie@sealedair.com>
2011-06-05 01:24:18 +0000

    added js standardization for date expansion

--- src/js/com.sealedair.mobile.xTemplates.js
+++ src/js/com.sealedair.mobile.xTemplates.js
@@ -2,7 +2,7 @@ Date.prototype.adjust = function(yr,mn,dy,hr,mi,se) {
 var m,t;
 this.setYear(this.getFullYear() + yr);
 m = this.getMonth() + mn;
-if(m != 0) this.setYear(this.getFullYear() + Math.floor(m/12));
+if(m !== 0) this.setYear(this.getFullYear() + Math.floor(m/12));
 if(m < 0) {
 this.setMonth(12 + (m%12));
 } else if(m > 0) {
